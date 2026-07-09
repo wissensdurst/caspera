c44b6ba7e4609b3d100c537a9eab0bcbb58a9811
Tomislav Ljubicic <wissensdurst@users.noreply.github.com>
2026-07-09 17:48:22 +0200

    Tu smo promjena

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,4 +5,7 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID ?? process.env.SANITY_PROJECT_ID ?? "nuwdwps7",
     dataset: process.env.SANITY_STUDIO_DATASET ?? process.env.SANITY_DATASET ?? "production",
   },
+  deployment: {
+    appId: "aj1w7qk0l0dsix34c7kichxa",
+  },
 });
